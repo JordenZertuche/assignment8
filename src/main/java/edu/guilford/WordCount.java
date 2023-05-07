2054decfa16550dd060fd8908ec90a2d034ae4ce
Spangley1 <mrtophatmangaming@gmail.com>
2023-05-07 12:50:04 -0400

    Final Version

--- src/main/java/edu/guilford/WordCount.java
+++ src/main/java/edu/guilford/WordCount.java
@@ -20,14 +20,11 @@ public class WordCount {
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader(filename));
-            HashMap<String, Integer> wordCount = new HashMap<>();
-            // creates a new HashMap object called wordCount that
-            // stores key-value pairs of type String and Integer
+            HashMap<String, Integer> wordCount = new HashMap<>(); 
+            // creates a new HashMap object called wordCount that stores key-value pairs of type String and Integer
 
             String line = reader.readLine();
-            // reads each line of the input text file, converts it to lower case, removes
-            // all non-alphanumeric characters using the regular expression [^a-zA-Z0-9 ],
-            // and then splits the line into individual words
+            // reads each line of the input text file, converts it to lower case, removes all non-alphanumeric characters using the regular expression [^a-zA-Z0-9 ], and then splits the line into individual words
             while (line != null) {
                 String[] words = line.toLowerCase().replaceAll("[^a-zA-Z0-9 ]", "").split("\\s+");
                 for (String word : words) {
@@ -41,19 +38,14 @@ public class WordCount {
             }
             reader.close();
             // This code takes the entry set of the wordCount hashmap and converts it to a
-            // List of Map.Entry objects,
-            // where each entry represents a key-value pair. Then it sorts the list based on
-            // the keys (words) in natural order
-            // using the comparingByKey() method of the Map.Entry class. This method returns
-            // a Comparator that compares the keys of the Map.Entry objects. The sorted list
-            // is then used to write the words and their counts to the output file.
+            // List of Map.Entry objects, where each entry represents a key-value pair. Then it sorts the list based on
+            // the keys (words) in natural order using the comparingByKey() method of the Map.Entry class. This method returns
+            // a Comparator that compares the keys of the Map.Entry objects. The sorted list is then used to write the words and their counts to the output file.
             List<Map.Entry<String, Integer>> sortedList = new ArrayList<>(wordCount.entrySet());
             sortedList.sort(Map.Entry.comparingByKey());
 
-            FileWriter writer = new FileWriter("src/main/resources/wordlist.txt");
-            // gets the occurrence count of each
-            // word and writes the word and its
-            // count to the output file
+            FileWriter writer = new FileWriter("src/main/resources/wordlist.txt"); 
+            // gets the occurrence count of each word and writes the word and its count to the output file.
             for (Map.Entry<String, Integer> entry : sortedList) {
                 writer.write(entry.getKey() + " " + entry.getValue() + "\n");
             }
@@ -64,11 +56,8 @@ public class WordCount {
             System.out.print("Enter a word to search for: ");
             String searchWord = scanner.nextLine().toLowerCase();
 
-            // Algorithm for Finding Number of Occurrences of a Word
-            // To find the number of occurrences of a specific word, the word is used as a
-            // key to retrieve its count from the hash table.
-            // If the word is not found in the hash table, then it does not appear in the
-            // text file.
+            // Algorithm for Finding Number of Occurrences of a Word To find the number of occurrences of a specific word, the word is used as a ey to retrieve its count from the hash table.
+            // If the word is not found in the hash table, then it does not appear in the text file.
             if (wordCount.containsKey(searchWord)) {
 
                 int count = wordCount.get(searchWord);
